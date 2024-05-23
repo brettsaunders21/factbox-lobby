6e13040edb58ac0f06820ddbb66f0ff7087dc2a8
Brett Saunders <brett_saunders@btinternet.com>
2024-05-23 21:25:37 +0000

    add port

--- api/index.ts
+++ api/index.ts
@@ -8,10 +8,11 @@ const server = https.createServer({
   key: fs.readFileSync('./private-key.pem'),
   cert: fs.readFileSync('./certificate.pem')
 }, (req, res) => {
-  
+  res.writeHead(200);
+  res.end("This is an HTTPS server supporting WebSocket.");
 });
 
-const wss = new WebSocket.Server({ port, server });
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function message(data) {
@@ -21,3 +22,6 @@ wss.on("connection", function connection(ws) {
   ws.send("something");
 });
 
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
